006aba1733422cc0e8c2eb31abc7ef205bf6ecb8
sackham0702 <sackham0702@gmail.com>
2023-12-02 21:37:01 +0900

    test finished!

--- src/main/java/SSDStore.java
+++ src/main/java/SSDStore.java
@@ -1,17 +1,20 @@
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public class SSDStore {
 	private Capacity capacity;
 	private SiteA siteA;
+	private SiteWrapper siteWrapper;
 	private ItemsByCapacity itemsByCapacity;
 	private Price price;
 	private ItemsByPrice itemsByPrice;
 	public SSDStore() {
 		this.capacity = new Capacity();
 		this.siteA = new SiteA();
-		this.itemsByCapacity = new ItemsByCapacity(capacity, siteA);
+		this.siteWrapper = new SiteWrapper(siteA);
+		this.itemsByCapacity = new ItemsByCapacity(capacity, siteWrapper);
 		this.price = new Price();
-		this.itemsByPrice = new ItemsByPrice(price, siteA);
+		this.itemsByPrice = new ItemsByPrice(price, siteWrapper);
 	}
 	public int getCapacity() {
 		return capacity.getValue();
