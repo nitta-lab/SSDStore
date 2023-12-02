ecc3c04747b50a37dcc6d9a1961d044afecb2d9b
e3LodeRunner <ecubeloderunner@gmail.com>
2023-12-02 15:11:05 +0900

    SSDStore のリファクタリング課題

--- src/main/java/ItemsByPrice.java
+++ src/main/java/ItemsByPrice.java
@@ -1,16 +1,18 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ItemsByPrice {
 	private Price price;
-	private SiteA siteA;
-	public ItemsByPrice(Price price, SiteA siteA) {
+	private SiteWrapper siteWrapper;
+	public ItemsByPrice(Price price, SiteWrapper siteWrapper) {
 		this.price = price;
-		this.siteA = siteA;
+		this.siteWrapper = siteWrapper;
 	}
 	public List<Map<String, Object>> getValue() {
 		List<Map<String, Object>> temp_l1 = new ArrayList<>();
 		{
-			for (Map<String, Object> item: this.siteA.getValue()) {
+			for (Map<String, Object> item: this.siteWrapper.getSiteValue()) {
 				if ((Integer) item.get("price") <= this.price.getValue()) {
 					temp_l1.add(item);
 				}
--- src/main/java/SSDStore.java
+++ src/main/java/SSDStore.java
@@ -1,17 +1,19 @@
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class SSDStore {
 	private Capacity capacity;
-	private SiteA siteA;
+	private SiteWrapper siteWrapper;
 	private ItemsByCapacity itemsByCapacity;
 	private Price price;
 	private ItemsByPrice itemsByPrice;
 	public SSDStore() {
 		this.capacity = new Capacity();
-		this.siteA = new SiteA();
-		this.itemsByCapacity = new ItemsByCapacity(capacity, siteA);
+		this.siteWrapper = new SiteWrapper(new SiteA());
+		this.itemsByCapacity = new ItemsByCapacity(capacity, siteWrapper);
 		this.price = new Price();
-		this.itemsByPrice = new ItemsByPrice(price, siteA);
+		this.itemsByPrice = new ItemsByPrice(price, siteWrapper);
 	}
 	public int getCapacity() {
 		return capacity.getValue();
@@ -19,11 +21,14 @@ public class SSDStore {
 	public void setCapacity(int cur_capacity) {
 		this.capacity.setCapacity(cur_capacity);
 	}
-	public List<Map<String, Object>> getSiteA() {
-		return siteA.getValue();
+	public List<Map<String, Object>> getSiteWrapper() {
+		if(Objects.isNull(siteWrapper)) {
+			return null;
+		}
+		return siteWrapper.getSiteValue();
 	}
 	public void addProductToSiteA(int capacity, int price) {
-		this.siteA.addProductToSiteA(capacity, price);
+		this.siteWrapper.addProductToSiteA(capacity, price);
 	}
 	public List<Map<String, Object>> getItemsByCapacity() {
 		return itemsByCapacity.getValue();
