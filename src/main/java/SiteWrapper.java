a8e8bb54c488bea731e3566a5eca95f452bb1dd3
e3LodeRunner <ecubeloderunner@gmail.com>
2023-12-03 11:00:30 +0900

    SSDStore機能追加課題

--- src/main/java/SiteWrapper.java
+++ src/main/java/SiteWrapper.java
@@ -1,11 +1,16 @@
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public class SiteWrapper {
 	private SiteA siteA;
-	public SiteWrapper(SiteA siteA) {
+	private SiteB siteB;
+	public SiteWrapper(SiteA siteA, SiteB siteB) {
 		this.siteA = siteA;
+		this.siteB = siteB;
 	}
-	public List<Map<String, Object>> getSiteAValue() {
-		return this.siteA.getValue();
+	public List<Map<String, Object>> getSiteValue() {
+		List<Map<String, Object>> productList = this.siteA.getValue();
+		productList.addAll(this.siteB.getValue());
+		return productList;
 	}
 }
