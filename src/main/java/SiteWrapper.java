217e2612090c08170642e28779914046ac413377
Takashi Sasaki <tsasaki609@gmail.com>
2023-12-03 10:02:29 +0900

    refactoring

---
+++ src/main/java/SiteWrapper.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.Map;
+
+public class SiteWrapper {
+  private SiteA siteA;
+  public SiteWrapper(SiteA siteA) {
+    this.siteA = siteA;
+  }
+  public List<Map<String, Object>> getSiteValue() {
+    return this.siteA.getValue();
+  }
+}
