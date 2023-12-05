f6c50eefd137d9444b2834ccf3fbce3f90152921
Joe Sakura <josephtessel@gmail.com>
2023-12-05 22:51:13 +0800

    ツール課題

--- src/main/java/ItemsByPrice.java
+++ src/main/java/ItemsByPrice.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class ItemsByPrice {
+	private SiteWrapper siteWrapper;
 	private Price price;
-	private SiteA siteA;
-	public ItemsByPrice(Price price, SiteA siteA) {
+	public ItemsByPrice(SiteWrapper siteWrapper, Price price) {
+		this.siteWrapper = siteWrapper;
 		this.price = price;
-		this.siteA = siteA;
 	}
 	public List<Map<String, Object>> getValue() {
 		List<Map<String, Object>> temp_l1 = new ArrayList<>();
 		{
-			for (Map<String, Object> item: this.siteA.getValue()) {
+			for (Map<String, Object> item: this.siteWrapper.getSiteAValue()) {
 				if ((Integer) item.get("price") <= this.price.getValue()) {
 					temp_l1.add(item);
 				}
