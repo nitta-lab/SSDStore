bb02d28da740c1d8f50add91bcbf1642a876f2b8
tsdyk <yk.tsd.jpn@gmail.com>
2023-12-10 20:01:08 +0900

    ツール課題

--- src/main/java/SSDStore.java
+++ src/main/java/SSDStore.java
@@ -1,40 +1,42 @@
 import java.util.*;
 
 public class SSDStore {
-	private Capacity capacity;
-	private SiteA siteA;
-	private ItemsByCapacity itemsByCapacity;
 	private Price price;
+	private SiteA siteA;
+	private SiteWrapper siteWrapper;
 	private ItemsByPrice itemsByPrice;
+	private Capacity capacity;
+	private ItemsByCapacity itemsByCapacity;
 	public SSDStore() {
-		this.capacity = new Capacity();
-		this.siteA = new SiteA();
-		this.itemsByCapacity = new ItemsByCapacity(capacity, siteA);
 		this.price = new Price();
-		this.itemsByPrice = new ItemsByPrice(price, siteA);
-	}
-	public int getCapacity() {
-		return capacity.getValue();
-	}
-	public void setCapacity(int cur_capacity) {
-		this.capacity.setCapacity(cur_capacity);
-	}
-	public List<Map<String, Object>> getSiteA() {
-		return siteA.getValue();
-	}
-	public void addProductToSiteA(int capacity, int price) {
-		this.siteA.addProductToSiteA(capacity, price);
-	}
-	public List<Map<String, Object>> getItemsByCapacity() {
-		return itemsByCapacity.getValue();
+		this.siteA = new SiteA();
+		this.siteWrapper = new SiteWrapper(siteA);
+		this.itemsByPrice = new ItemsByPrice(price, siteWrapper);
+		this.capacity = new Capacity();
+		this.itemsByCapacity = new ItemsByCapacity(capacity, siteWrapper);
 	}
 	public int getPrice() {
 		return price.getValue();
 	}
-	public void setPrice(int cur_price) {
-		this.price.setPrice(cur_price);
+	public List<Map<String, Object>> getSiteA() {
+		return siteA.getValue();
 	}
 	public List<Map<String, Object>> getItemsByPrice() {
 		return itemsByPrice.getValue();
+	}
+	public int getCapacity() {
+		return capacity.getValue();
+	}
+	public List<Map<String, Object>> getItemsByCapacity() {
+		return itemsByCapacity.getValue();
+	}
+	public void addProductToSiteA(int capacity, int price) {
+		this.siteA.addProductToSiteA(capacity, price);
+	}
+	public void setCapacity(int cur_capacity) {
+		this.capacity.setCapacity(cur_capacity);
+	}
+	public void setPrice(int cur_price) {
+		this.price.setPrice(cur_price);
 	}
 }
