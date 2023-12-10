b1432c9ba046b85aad2f59f130c8a7a013c616be
Akitosh <idmitryvdev@gmail.com>
2023-12-11 00:25:20 +0800

    ツール課題

--- src/main/java/SSDStore.java
+++ src/main/java/SSDStore.java
@@ -1,40 +1,42 @@
 import java.util.*;
 
 public class SSDStore {
+	private Price price;
 	private Capacity capacity;
 	private SiteA siteA;
+	private SiteWrapper siteWrapper;
 	private ItemsByCapacity itemsByCapacity;
-	private Price price;
 	private ItemsByPrice itemsByPrice;
 	public SSDStore() {
+		this.price = new Price();
 		this.capacity = new Capacity();
 		this.siteA = new SiteA();
-		this.itemsByCapacity = new ItemsByCapacity(capacity, siteA);
-		this.price = new Price();
-		this.itemsByPrice = new ItemsByPrice(price, siteA);
-	}
-	public int getCapacity() {
-		return capacity.getValue();
-	}
-	public void setCapacity(int cur_capacity) {
-		this.capacity.setCapacity(cur_capacity);
-	}
-	public List<Map<String, Object>> getSiteA() {
-		return siteA.getValue();
-	}
-	public void addProductToSiteA(int capacity, int price) {
-		this.siteA.addProductToSiteA(capacity, price);
-	}
-	public List<Map<String, Object>> getItemsByCapacity() {
-		return itemsByCapacity.getValue();
+		this.siteWrapper = new SiteWrapper(siteA);
+		this.itemsByCapacity = new ItemsByCapacity(capacity, siteWrapper);
+		this.itemsByPrice = new ItemsByPrice(siteWrapper, price);
 	}
 	public int getPrice() {
 		return price.getValue();
 	}
-	public void setPrice(int cur_price) {
-		this.price.setPrice(cur_price);
+	public int getCapacity() {
+		return capacity.getValue();
+	}
+	public List<Map<String, Object>> getSiteA() {
+		return siteA.getValue();
+	}
+	public List<Map<String, Object>> getItemsByCapacity() {
+		return itemsByCapacity.getValue();
 	}
 	public List<Map<String, Object>> getItemsByPrice() {
 		return itemsByPrice.getValue();
+	}
+	public void setCapacity(int cur_capacity) {
+		this.capacity.setCapacity(cur_capacity);
+	}
+	public void addProductToSiteA(int capacity, int price) {
+		this.siteA.addProductToSiteA(capacity, price);
+	}
+	public void setPrice(int cur_price) {
+		this.price.setPrice(cur_price);
 	}
 }
