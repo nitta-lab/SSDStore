0164b89672d9de954a6561da6f2aef6465cc043b
sinkovmikhail <sinkovmikhail@outlook.com>
2023-12-14 18:41:22 +0900

    ツール課題

--- src/main/java/SSDStore.java
+++ src/main/java/SSDStore.java
@@ -1,29 +1,25 @@
 import java.util.*;
 
 public class SSDStore {
-	private Capacity capacity;
 	private SiteA siteA;
+	private Capacity capacity;
+	private SiteWrapper siteWrapper;
 	private ItemsByCapacity itemsByCapacity;
 	private Price price;
 	private ItemsByPrice itemsByPrice;
 	public SSDStore() {
-		this.capacity = new Capacity();
 		this.siteA = new SiteA();
-		this.itemsByCapacity = new ItemsByCapacity(capacity, siteA);
+		this.capacity = new Capacity();
+		this.siteWrapper = new SiteWrapper(siteA);
+		this.itemsByCapacity = new ItemsByCapacity(capacity, siteWrapper);
 		this.price = new Price();
-		this.itemsByPrice = new ItemsByPrice(price, siteA);
-	}
-	public int getCapacity() {
-		return capacity.getValue();
-	}
-	public void setCapacity(int cur_capacity) {
-		this.capacity.setCapacity(cur_capacity);
+		this.itemsByPrice = new ItemsByPrice(price, siteWrapper);
 	}
 	public List<Map<String, Object>> getSiteA() {
 		return siteA.getValue();
 	}
-	public void addProductToSiteA(int capacity, int price) {
-		this.siteA.addProductToSiteA(capacity, price);
+	public int getCapacity() {
+		return capacity.getValue();
 	}
 	public List<Map<String, Object>> getItemsByCapacity() {
 		return itemsByCapacity.getValue();
@@ -31,10 +27,16 @@ public class SSDStore {
 	public int getPrice() {
 		return price.getValue();
 	}
+	public List<Map<String, Object>> getItemsByPrice() {
+		return itemsByPrice.getValue();
+	}
 	public void setPrice(int cur_price) {
 		this.price.setPrice(cur_price);
 	}
-	public List<Map<String, Object>> getItemsByPrice() {
-		return itemsByPrice.getValue();
+	public void setCapacity(int cur_capacity) {
+		this.capacity.setCapacity(cur_capacity);
+	}
+	public void addProductToSiteA(int capacity, int price) {
+		this.siteA.addProductToSiteA(capacity, price);
 	}
 }
